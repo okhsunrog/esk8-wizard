53a1b0b35af45ebf7aad3fcdde713fb2e655197b
Данила Горнушко <me@okhsunrog.ru>
2023-09-26 16:13:44 +0300

    fix can.cpp

--- main/can.cpp
+++ main/can.cpp
@@ -11,7 +11,7 @@ static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
 static const twai_filter_config_t f_config = {
     .acceptance_code = 0, .acceptance_mask = 0, .single_filter = true};
 static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(
-    CONFIG_CAN_TX_GPIO, CONFIG_CAN_RX_GPIO, TWAI_MODE_NORMAL);
+    (gpio_num_t) CONFIG_CAN_TX_GPIO, (gpio_num_t) CONFIG_CAN_RX_GPIO, TWAI_MODE_NORMAL);
 
 static const char *LOG_TAG = "can";
 
@@ -32,7 +32,8 @@ void can_stop() {
 }
 
 void send_command(uint8_t command_id, uint8_t *data4) {
-    twai_message_t tx_msg = {.extd = 1};
+    twai_message_t tx_msg = {};
+    tx_msg.extd = 1;
     tx_msg.identifier = VESC_ID + (command_id << 8);
     tx_msg.data_length_code = 4;
     for (int i = 0; i < 4; i++) {
